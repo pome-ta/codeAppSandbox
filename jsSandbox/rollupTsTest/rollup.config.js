7013bf4257bda13e836072c724f09a4a77f70e69
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-23 21:49:52 +0900

    hummmm....

--- jsSandbox/rollupTsTest/rollup.config.js
+++ jsSandbox/rollupTsTest/rollup.config.js
@@ -3,8 +3,8 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: './src/app.ts',
   output: {
-    file: 'bundle.js',
-    format: 'iife',
+    file: './bundle.js',
+    format: 'es',
   },
   plugins: [
     typescript()
