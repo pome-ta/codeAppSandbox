8c29164ee72f3fb668a413c17dcfebefc55d32ee
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-30 00:55:38 +0900

    init

---
+++ p5Sandbox/andSoundV1/docs/js/srcBundles/p5SoundBundles.js
@@ -0,0 +1,1 @@
+export * as p5sound from 'p5/lib/addons/p5.sound.js';
---
+++ p5Sandbox/andSoundV1/rollup.config.js
@@ -0,0 +1,27 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const p5Set = () => {
+  return {
+    input: './docs/js/srcBundles/p5Bundles.js',
+    output: {
+      file: './docs/js/lib/p5.bundle.js',
+      format: 'esm',
+    },
+    plugins: [nodeResolve(), commonjs(),],
+  };
+};
+
+const soundSet = () => {
+  return {
+    input: './docs/js/srcBundles/p5SoundBundles.js',
+    output: {
+      file: './docs/js/lib/addons/p5sound.bundle.js',
+      format: 'esm',
+    },
+    plugins: [nodeResolve(), commonjs(),],
+  };
+};
+
+
+export default [p5Set(), soundSet(),];
