2b868fb570ac14fbcc4ed2c7980cb54c48bad3a8
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-29 15:25:43 +0900

    not w

---
+++ p5Sandbox/scndPlayGround/docs/js/p5.bundle.js
@@ -0,0 +1,1 @@
+export { p5 } from 'p5/lib/p5.min.js';
--- p5Sandbox/scndPlayGround/rollup.config.js
+++ p5Sandbox/scndPlayGround/rollup.config.js
@@ -1,49 +1,27 @@
-import { copy } from '@web/rollup-plugin-copy';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import localResolve from 'rollup-plugin-local-resolve';
+import copy from 'rollup-plugin-copy'
 
 
 const p5Set = () => {
   return {
     input: './docs/js/bundles/p5bundle.js',
+    /*
     output: {
-      dir: './docs/lib',
-      format: 'es',
+      file: './docs/js/p5.bundle.js',
+      format: 'esm',
     },
-    plugins: [
-      copy({ patterns: '**/*.js', rootDir: 'node_modules/p5/lib' });
-    ],
-  };
-};
-
-//console.log('m');
-
-export default [p5Set()];
-
-
-/*
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import copy from 'rollup-plugin-copy'
-
-const p5Set = () => {
-  return {
-    input: './js/bundles/rollupInput/p5Input.js',
-    // output: {
-    //   file: './js/bundles/p5.bundle.js',
-    //   format: 'esm',
-    // },
-  plugins: [nodeResolve(), commonjs(),
-    copy({
-        targets: [
-        //   { src: 'node_modules/p5/lib', dest: './js/bundles/' },
-          { src: 'node_modules/p5/lib/p5.esm.js', dest: './js/bundles/' },
-        //   { src: './js/bundles/p5.bundle.js', dest: './js/bundles/fuga' },
-        ]
-      })
+    */
+  plugins: [nodeResolve(), commonjs(), localResolve(),
+  copy({
+      targets: [
+        { src: 'node_modules/p5/lib', dest: './docs' },
+      ]
+    }),
   ],
   };
 };
 
-
-
 export default [p5Set()];
-*/
+
