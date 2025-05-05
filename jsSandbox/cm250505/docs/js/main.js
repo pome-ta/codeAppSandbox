af81e793aaa9c4c66aee6d44f4c3308d31f48bd4
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-05-05 17:12:12 +0900

    set up

--- jsSandbox/cm250505/docs/js/main.js
+++ jsSandbox/cm250505/docs/js/main.js
@@ -6,136 +6,72 @@ import {
   StateEffect,
   Decoration,
   initExtensions,
-  editorDiv,
 } from './modules/codemirror.bundle.js';
 
+
+let editor;
+
 /* --- main */
-const container = document.createElement('main');
-container.id = 'container-main';
-container.style.height = '100dvh';
-container.style.backgroundColor = 'maroon';
+// --- wrap
+const setupWrap = () => {
 
-// const btn = document.createElement('div');
-// btn.textContent = 'underline';
-// btn.style.height = '3rem';
 
-// container.appendChild(btn);
+  const container = document.createElement('main');
+  container.id = 'container-main';
+  container.style.height = '100dvh';
+  // container.style.backgroundColor = 'maroon';
 
-// document.body.appendChild(container).appendChild(editorDiv);
+  const editorDiv = document.createElement('div');
+  editorDiv.id = 'editor-div';
+  editorDiv.style.width = '100%';
+  editorDiv.style.height = '100dvh';
+  // editorDiv.style.backgroundColor = 'darkseagreen';
 
-const bgRectangleClassName = 'cm-bgRectangle';
+  document.body.appendChild(container).appendChild(editorDiv);
+  return editorDiv;
 
-const bgRectEffect = {
-  add: StateEffect.define({ from: 0, to: 0 }),
-  remove: StateEffect.define({ from: 0, to: 0 }),
-};
-
-const bgRectangleField = StateField.define({
-  create() {
-    return Decoration.none;
-  },
-  update(bgRectangles, tr) {
-    bgRectangles = bgRectangles.map(tr.changes);
-    for (const ef of tr.effects) {
-      if (ef.is(bgRectEffect.add)) {
-        bgRectangles = bgRectangles.update({
-          add: [bgRectangleMark.range(ef.value.from, ef.value.to)],
-        });
-      } else if (ef.is(bgRectEffect.remove)) {
-        bgRectangles = bgRectangles.update({
-          // filter: (from, to, value) => {
-          //   let shouldRemove =
-          //     from === e.value.from &&
-          //     to === e.value.to &&
-          //     value.spec.class === bgRectangleClassName;
-          //   return !shouldRemove;
-          // },
-          filter: (f, t, value) => !(value.class === bgRectangleClassName),
-        });
-      }
-    }
-    return bgRectangles;
-  },
-  provide: (f) => EditorView.decorations.from(f),
-});
-
-const bgRectangleMark = Decoration.mark({ class: bgRectangleClassName });
-const bgRectangleTheme = EditorView.baseTheme({
-  '.cm-bgRectangle': { backgroundColor: '#23232380' },
-});
-
-function bgRectangleSet(view) {
-  const { state, dispatch } = view;
-  const { from, to } = state.selection.main.extend(0, state.doc.length);
-  const decoSet = state.field(bgRectangleField, false);
-
-  const addFromTO = (from, to) => bgRectEffect.add.of({ from, to });
-  const removeFromTO = (from, to) => bgRectEffect.remove.of({ from, to });
-
-  let effects = [];
-  effects.push(
-    !decoSet ? StateEffect.appendConfig.of([bgRectangleField]) : null
-  );
-  decoSet?.between(from, to, (decoFrom, decoTo) => {
-    if (from === decoTo || to === decoFrom) {
-      return;
-    }
-    effects.push(removeFromTO(from, to));
-    effects.push(removeFromTO(decoFrom, decoTo));
-    effects.push(decoFrom < from ? addFromTO(decoFrom, from) : null);
-    effects.push(decoTo > to ? addFromTO(to, decoTo) : null);
-  });
-
-  effects.push(addFromTO(from, to));
-
-  if (!effects.length) {
-    return false;
-  }
-  dispatch({ effects: effects.filter((ef) => ef) });
-  return true;
 }
 
-const updateCallBack = EditorView.updateListener.of(
-  (update) => update.docChanged && updateDocs(update)
-);
 
-function updateDocs(view) {
-  bgRectangleSet(editor);
-}
 
-const resOutlineTheme = EditorView.baseTheme({
-  '&.cm-editor': {
-    '&.cm-focused': {
-      outline: '0px dotted #212121',
+const myTheme = EditorView.theme(
+  {
+    '&': {
+      fontSize: '0.72rem',
+      // fontSize: '1rem',
     },
+    '.cm-scroller': {
+      fontFamily:
+        'Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace',
+    },
+    '.cm-line': { padding: 0 },
   },
-});
+  { dark: true }
+);
 
 const extensions = [
   ...initExtensions,
-  // resOutlineTheme,
-  bgRectangleTheme,
-  updateCallBack,
+  myTheme,
 ];
-const docText = `hoge fuga あああああ
-ほげほげ、ふががう
 
-hoge i0oialuwOlL1`;
+// --- EditorView
+const viewEditor = () => {
+  // document.body.appendChild(container).appendChild(editorDiv);
+  const editorDiv = setupWrap();
 
-const state = EditorState.create({
-  doc: docText,
-  extensions: extensions,
+  const state = EditorState.create({
+    extensions: extensions,
+    doc: 'ほげほげ',
+  })
+  editor = new EditorView({
+    state,
+    parent: editorDiv,
+  })
+
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  viewEditor();
+
 });
-
-const editor = new EditorView({
-  state,
-  parent: editorDiv,
-});
-
-bgRectangleSet(editor);
-
-
-document.addEventListener('DOMContentLoaded', ()=> {
-  document.body.appendChild(container).appendChild(editorDiv);
-
-})
--- jsSandbox/cm250505/docs/js/modules/srcBundles/codemirror.setup.js
+++ jsSandbox/cm250505/docs/js/modules/srcBundles/codemirror.setup.js
@@ -25,30 +25,30 @@ import {
   cursorCharRight,
   toggleComment,
 } from '@codemirror/commands';
-import { closeBrackets } from '@codemirror/autocomplete';
+import { closeBrackets, autocompletion } from '@codemirror/autocomplete';
 import { bracketMatching } from '@codemirror/language';
-// import { javascript } from '@codemirror/lang-javascript';
+import { javascript } from '@codemirror/lang-javascript';
 //import { cpp } from '@codemirror/lang-cpp';
 //import { oneDark } from './theme-custom-dark.js';
 
-const editorDiv = document.createElement('div');
-editorDiv.id = 'editor-div';
-editorDiv.style.width = '100%';
+// const editorDiv = document.createElement('div');
+// editorDiv.id = 'editor-div';
+// editorDiv.style.width = '100%';
 
-const myTheme = EditorView.theme(
-  {
-    '&': {
-      // fontSize: '0.72rem',
-      fontSize: '1rem',
-    },
-    '.cm-scroller': {
-      fontFamily:
-        'Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace',
-    },
-    '.cm-line': { padding: 0 },
-  },
-  { dark: true }
-);
+// const myTheme = EditorView.theme(
+//   {
+//     '&': {
+//       // fontSize: '0.72rem',
+//       fontSize: '1rem',
+//     },
+//     '.cm-scroller': {
+//       fontFamily:
+//         'Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace',
+//     },
+//     '.cm-line': { padding: 0 },
+//   },
+//   { dark: true }
+// );
 
 const tabSize = new Compartment();
 
@@ -57,14 +57,15 @@ const initExtensions = [
   lineNumbers(),
   highlightActiveLineGutter(),
   highlightActiveLine(),
+  autocompletion(),
   closeBrackets(),
   bracketMatching(),
   EditorView.lineWrapping, // 改行
   tabSize.of(EditorState.tabSize.of(2)),
-  // javascript(),
+  javascript(),
   //cpp(),
   //oneDark,
-  myTheme,
+  // myTheme,
 ];
 
 export {
@@ -86,5 +87,5 @@ export {
   cursorCharRight,
   toggleComment,
   initExtensions,
-  editorDiv,
+  // editorDiv,
 };
