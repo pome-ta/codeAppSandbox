bb78ff233bff72d5816e9cb41da3f2edde89327d
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-05-06 00:40:37 +0900

    up

--- jsSandbox/cm250505/docs/js/main.js
+++ jsSandbox/cm250505/docs/js/main.js
@@ -19,13 +19,13 @@ const setupWrap = () => {
   const container = document.createElement('main');
   container.id = 'container-main';
   container.style.height = '100dvh';
-  // container.style.backgroundColor = 'maroon';
+  container.style.backgroundColor = 'maroon';
 
   const editorDiv = document.createElement('div');
   editorDiv.id = 'editor-div';
   editorDiv.style.width = '100%';
   editorDiv.style.height = '100dvh';
-  // editorDiv.style.backgroundColor = 'darkseagreen';
+  editorDiv.style.backgroundColor = 'darkseagreen';
 
   document.body.appendChild(container).appendChild(editorDiv);
   return editorDiv;
@@ -37,8 +37,8 @@ const setupWrap = () => {
 const myTheme = EditorView.theme(
   {
     '&': {
-      fontSize: '0.72rem',
-      // fontSize: '1rem',
+      //fontSize: '0.72rem',
+      fontSize: '1rem',
     },
     '.cm-scroller': {
       fontFamily:
