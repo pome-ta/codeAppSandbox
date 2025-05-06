e2162b864b0f59461c9ca0d836a8114d4009a548
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-05-06 13:52:21 +0900

    sandbox

--- jsSandbox/cm250505/docs/js/main.js
+++ jsSandbox/cm250505/docs/js/main.js
@@ -11,6 +11,78 @@ import {
 
 let editor;
 
+
+// sandbox
+const sandbox = document.createElement('iframe');
+sandbox.id = 'sandbox'
+sandbox.src = './sandboxes/sandbox.html'
+sandbox.style.display = 'none'
+
+const controller = document.createElement('div');
+controller.id = 'controller'
+
+const runCode = document.createElement('button');
+runCode.id = 'runCode'
+runCode.textContent = 'runCode'
+
+const clearConsole = document.createElement('button');
+clearConsole.id ='clearConsole'
+clearConsole.textContent = 'clearConsole'
+
+controller.appendChild(runCode)
+controller.appendChild(clearConsole)
+
+
+
+
+
+/**
+ * boxConsole(実行結果table)
+ */
+const boxConsole = document.createElement('div');
+boxConsole.id = 'boxConsole'
+
+const tbl = document.createElement('table');
+const tblBody = document.createElement('tbody');
+
+const setupTable = () => {
+  tbl.appendChild(tblBody);
+  boxConsole.appendChild(tbl);
+};
+
+const addTableRow = (time_success_code) => {
+  let issuccess = false;
+  const row = document.createElement('tr');
+
+  for (const [key, value] of Object.entries(time_success_code)) {
+    const cell = document.createElement('td');
+    const cellText = document.createElement('code');
+    cellText.innerText = value;
+
+    if (key === 'success') {
+      issuccess = value;
+      cellText.innerText = issuccess ? '🟢' : '❌';
+    }
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+  }
+  issuccess ? row.classList.add('success') : row.classList.add('noSuccess');
+  tblBody.prepend(row);
+};
+
+const removeTableRow = () => {
+  while (tblBody.firstChild) {
+    tblBody.removeChild(tblBody.firstChild);
+  }
+};
+
+document.body.appendChild(controller)
+document.body.appendChild(sandbox)
+document.body.appendChild(boxConsole)
+setupTable();
+
+
+
 /* --- main */
 // --- wrap
 const setupWrap = () => {
@@ -26,6 +98,7 @@ const setupWrap = () => {
   editorDiv.style.width = '100%';
   editorDiv.style.height = '100dvh';
   //editorDiv.style.backgroundColor = 'darkseagreen';
+  
 
   document.body.appendChild(container).appendChild(editorDiv);
   return editorDiv;
@@ -73,5 +146,7 @@ const viewEditor = () => {
 
 document.addEventListener('DOMContentLoaded', () => {
   viewEditor();
+  
+  console.log(editor)
 
 });
---
+++ jsSandbox/cm250505/docs/js/sandboxes/sandbox.js
@@ -0,0 +1,32 @@
+//[extensionMonaco/src/sandbox/sandbox.js at main · pome-ta/extensionMonaco · GitHub](https://github.com/pome-ta/extensionMonaco/blob/main/src/sandbox/sandbox.js)
+
+
+let consoleResult;
+
+console.log = function (...log) {
+  consoleResult += log.map((lg) => JSON.stringify(lg)) + '\n';
+};
+
+window.addEventListener('message', (e) => {
+  consoleResult = '';
+  const timeStr = new Date().toLocaleTimeString();
+  const code = e.data;
+  try {
+    eval(code);
+    e.source.postMessage(
+      {
+        result: { time: timeStr, success: true, code: consoleResult },
+        isSandbox: true,
+      },
+      e.origin
+    );
+  } catch (error) {
+    e.source.postMessage(
+      {
+        result: { time: timeStr, success: false, code: error },
+        isSandbox: true,
+      },
+      e.origin
+    );
+  }
+});
