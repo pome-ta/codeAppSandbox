32ae9f12c31d2988975a5ddd7502407aaf2328ea
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-05-14 01:05:53 +0900

    up

---
+++ p5Sandbox/sb03/docs/js/app.js
@@ -0,0 +1,15 @@
+import './sketchMain.js';
+
+/*
+import('./sketchMain.js').then(module => {
+  console.log(module)
+});
+*/
+/*
+
+.then(module => {
+      // 動的に読み込まれたSubクラス
+      const sub = new module.Sub();
+      sub.subMethod();  
+    });
+*/
