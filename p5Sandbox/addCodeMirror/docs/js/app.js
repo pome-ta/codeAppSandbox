5267546fe5a2aa9d101488149a17eaa52e584c20
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-05-22 08:42:40 +0900

    sandbox

--- p5Sandbox/addCodeMirror/docs/js/app.js
+++ p5Sandbox/addCodeMirror/docs/js/app.js
@@ -1,5 +1,7 @@
 import Editor from './editor.js';
 
+
+
 const sketchCode = `
 function setup() {
   createCanvas(240, 240);
@@ -10,7 +12,16 @@ function draw() {
 }`;
 
 
+// sandbox
+const sandbox = document.createElement('iframe');
+sandbox.id = 'sandbox';
+sandbox.src = './js/sandboxes/sandbox.html';
+//sandbox.style.display = 'none';
+sandbox.style.width = '100%';
+sandbox.style.height = '50%';
+sandbox.style.backgroundColor = 'maroon';
 
+/*
 const sketchMain = document.createElement('main');
 //const sketchMain = document.createElement('div');
 sketchMain.id = 'sketchMain';
@@ -18,9 +29,16 @@ sketchMain.style.width = '100%';
 sketchMain.style.height = '50%';
 sketchMain.style.backgroundColor = 'maroon'
 
+
 const scriptElement = document.createElement('script');
 sketchMain.appendChild(scriptElement);
+*/
 
+
+const runButton = document.createElement('button');
+runButton.id = 'runButton'
+runButton.textContent = 'runCode'
+runButton.style.margin = '1rem';
 
 const editorDiv = document.createElement('div');
 editorDiv.id = 'editor-div';
@@ -28,17 +46,40 @@ editorDiv.style.width = '100%';
 
 document.body.style.backgroundColor = 'teal'
 
+/**
+ * sandbox へ投げるメッセージ
+ */
+const postMessage = () => {
+  const src = editor.doc
+  const jsBlob = new Blob([src], { type: 'text/javascript' });
+  const blobURL = URL.createObjectURL(jsBlob);
+  sandbox.contentWindow.postMessage(blobURL, '*');
+  
+  //sandbox.contentWindow.postMessage(src, '*');
+  console.log(src)
+};
+
+const editor = new Editor(editorDiv, sketchCode);
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(sketchMain);
+  //document.body.appendChild(sketchMain);
+  document.body.appendChild(sandbox);
+  document.body.appendChild(runButton);
   document.body.appendChild(editorDiv);
-  const editor = new Editor(editorDiv, sketchCode);
+  
+  
+  
   
   const jsBlob = new Blob([editor.doc], { type: 'text/javascript' })
   const blobURL = URL.createObjectURL(jsBlob);
-  scriptElement.src = blobURL;
+  //scriptElement.src = blobURL;
+  console.log(blobURL)
+  
+  
+  runButton.addEventListener('click', (e) => postMessage());
+  postMessage()
 
   
-  console.log(editor.doc)
+  //console.log(editor)
   
 });
